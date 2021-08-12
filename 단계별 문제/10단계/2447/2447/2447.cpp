4e53835a8afe661a56186c3745a4aa8e284e24ee
EunSe-o <dkan601@naver.com>
2021-08-12 18:20:56 +0900

    재귀

---
+++ 단계별 문제/10단계/2447/2447/2447.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void Star(int n)
+{
+	n / 3 + 1 부터 비우기
+
+		for (int col = 0;)
+			cout << "***" << endl << "* *" << endl << "***";
+	Star(n / 3);
+}
+
+int main(void)
+{
+	int natNum = 0;
+	cin >> natNum;
+
+	return 0;
+}
