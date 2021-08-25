3c14e06d3c8363a52102945b583a73916e4ca622
EunSe-o <dkan601@naver.com>
2021-08-25 21:21:26 +0900

    기본 수학1

--- 단계별 문제/8단계/1011/1011/1011.cpp
+++ 단계별 문제/8단계/1011/1011/1011.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main(void)
 {
+	int tCase = 0;
+	cin >> tCase;
 
+	for (int inputData = 0; inputData < tCase; inputData++)
+	{
+		int cnt = 0;
+		int now = 0, arr = 0;
+		cin >> now >> arr;
+		int dis = arr - now;
+		int move = 1;
+
+		while (dis > move * 2)
+		{
+			dis -= move * 2;
+			move++;
+			cnt += 2;
+		}
+
+		if (dis <= move)
+			cnt++;
+		else if (dis > move)
+			cnt += 2;
+
+		cout << cnt << endl;
+	}
 
 	return 0;
 }
