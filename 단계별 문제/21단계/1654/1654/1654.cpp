e1d2cac6707e07e8babcc221fcda7cb13ef9e90c
EunSe-o <dkan601@naver.com>
2021-08-27 18:47:51 +0900

    이분 탐색

--- 단계별 문제/21단계/1654/1654/1654.cpp
+++ 단계별 문제/21단계/1654/1654/1654.cpp
@@ -3,14 +3,36 @@ using namespace std;
 
 void MakeLine(int already, int need, int* leng)
 {
+	int sum = 0, shortest = leng[0];
+	for (int search = 0; search < already; search++)
+	{
+		sum += leng[search];
+		if (leng[search] < shortest)
+			shortest = leng[search];
+	}
 
+	int cnt = 0;
+	for (int cut = sum / need; cut <= sum / need; cut--)
+	{
+		cnt = 0;
+		for (int index = 0; index < already; index++)
+		{
+			cnt += leng[index] / cut;
+		}
+
+		if (cnt == need)
+		{
+			cout << cut;
+			break;
+		}
+	}
 }
 
 int main(void)
 {
 	int already = 0, need = 0;
 	cin >> already >> need;
-	
+
 	int* leng = new int[already];
 	for (int inputData = 0; inputData < already; inputData++)
 		cin >> leng[inputData];
